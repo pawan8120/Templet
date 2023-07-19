0d9435a7d015e75f880b591cc1c5ce4ed18cc6ae
pawan8120 <95530795+pawan8120@users.noreply.github.com>
2023-07-19 12:12:37 +0530

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mll map<ll,ll>
+#define sll set<ll>
+#define umll unordered_map<ll,ll>
+#define pb push_back
+#define vll vector<ll>
+#define pl pair<ll,ll>
+#define mod1 998244353
+#define mod2 1000000007
+#define el cout<<"\n"
+#define fi first 
+#define se second 
+#define lb lower_bound 
+#define ub upper_bound 
+#define sz size()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+ll binexp(ll a,ll b,ll mod){ll res=1;while(b){if(b&1){res=(res*a)%mod;}a=(a*a)%mod;b>>=1;}return res;}
+int main()
+{ 
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+ll t,x,y,var,p,q,n,m,k; 
+t=1; 
+cin>>t;
+while(t--){
+   
+    
+}   
+    return 0;
+}
